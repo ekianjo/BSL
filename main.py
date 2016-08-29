69b35105645e63cbce0afba1af0a710e9087af40
ekianjo <ekianjo@gmail.com>
2016-08-29 14:55:38 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#flow
+#general idea is to reduce the workload associated with adding games from different sources
+
+#bash script to install key components (wine, playonlinux, emulators,innoextract via associated PPAs)
+#find appid automatically from Steam installs in POL
+#appid finder to get steam grid image as well as game name from Steam
+#create shortcuts automatically for POL games using the info from appid finder
+#create shortcuts automatically for Emulator games and add proper controls
+#confirm if steam controller is working as expected for all emulators
+#GOG games installer -> create shortcut automatically as well
+
